f1afd358d4e8ed22d96c358880fe410048e082d0
Pavanpreet Gandhi <pavanpreet.gandhi@gmail.com>
2025-11-06 11:31:50 +0000

    refactoring for readability

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,17 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>
-}
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
-const Content = (props) => {
-  const parts = props.parts
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
+const Content = ({ parts }) => (
+  <div>
+    <Part name={parts[0].name} exercises={parts[0].exercises} />
+    <Part name={parts[1].name} exercises={parts[1].exercises} />
+    <Part name={parts[2].name} exercises={parts[2].exercises} />
+  </div>
+)
 
-const Total = (props) => {
-  let sum = props.parts.reduce((acc, part) => acc + part.exercises, 0);
+const Total = ({parts}) => {
+  let sum = parts.reduce((acc, part) => acc + part.exercises, 0);
   return (
     <p>Number of exercises {sum}</p>
   )
